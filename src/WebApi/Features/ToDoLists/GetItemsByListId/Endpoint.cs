61dae99578cf5ebbec9ffcf5b047f268ab0d7cde
Alessio <alessio.marchese148@gmail.com>
2024-05-23 16:33:58 +0200

    Aggiunto feature per recuperare tutti gli item di una todolist

---
+++ src/WebApi/Features/ToDoLists/GetItemsByListId/Endpoint.cs
@@ -0,0 +1,26 @@
+﻿using FastEndpoints;
+using Microsoft.EntityFrameworkCore;
+using webapi.Entities;
+using webapi.Infastructure.Data;
+
+namespace webapi.Features.ToDoLists.GetItemsByListId;
+
+internal sealed class Endpoint(ApplicationDbContext context, AutoMapper.IMapper mapper) : Endpoint<Request, Response>
+{
+    public override void Configure()
+    {
+        Get("/api/todo-list/{id}/todo-item");
+        AllowAnonymous();
+    }
+
+    public override async Task HandleAsync(Request r, CancellationToken c)
+    {
+        var toDoList = await context.ToDoLists.FirstOrDefaultAsync(x => x.Id == r.Id);
+        if (toDoList == null)
+        {
+            await SendNotFoundAsync(c);
+            return;
+        }
+        await SendAsync(new Response(toDoList.ToDoItems), cancellation: c);
+    }
+}
---
+++ src/WebApi/Features/ToDoLists/GetItemsByListId/Models.cs
@@ -0,0 +1,6 @@
+﻿using webapi.Entities;
+
+namespace webapi.Features.ToDoLists.GetItemsByListId;
+
+public record class Request(Guid Id);
+public record class Response(List<ToDoItem> ToDoItems);
