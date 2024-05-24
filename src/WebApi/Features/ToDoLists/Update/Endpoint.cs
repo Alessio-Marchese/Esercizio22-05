c21b0bc13921799808fbeaa529f1f98a4ac6d4b4
Alessio <alessio.marchese148@gmail.com>
2024-05-24 09:38:58 +0200

    Aggiunto API per l'update delle liste

---
+++ src/WebApi/Features/ToDoLists/Update/Endpoint.cs
@@ -0,0 +1,38 @@
+﻿using FastEndpoints;
+using webapi.Infastructure.Data;
+
+namespace webapi.Features.ToDoLists.Update
+{
+    internal sealed class Endpoint(ApplicationDbContext context) : Endpoint<Request, EmptyResponse>
+    {
+        public override void Configure()
+        {
+            Put("/api/todo-list/{id}");
+            AllowAnonymous();
+        }
+
+        public override async Task HandleAsync(Request r, CancellationToken c)
+        {
+            var toDoList = await context.ToDoLists.FindAsync(r.id);
+            if (toDoList is null)
+            {
+                await SendNotFoundAsync();
+                return;
+            }
+            if (!string.IsNullOrWhiteSpace(r.Title)) 
+            {
+                toDoList.Title = r.Title;
+            }
+            if(toDoList.IsDone != r.IsDone)
+            {
+                toDoList.IsDone = r.IsDone;
+            }
+            if(r.ToDoItems.Count > 0)
+            {
+                toDoList.ToDoItems = r.ToDoItems;
+            }
+            await context.SaveChangesAsync();
+            await SendNoContentAsync();
+        }
+    }
+}
---
+++ src/WebApi/Features/ToDoLists/Update/Models.cs
@@ -0,0 +1,7 @@
+﻿using webapi.Entities;
+
+namespace webapi.Features.ToDoLists.Update
+{
+public record class Request(Guid id, string Title, bool IsDone, List<ToDoItem> ToDoItems);
+public record class Response;
+}
