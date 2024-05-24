2106274d8acd2c8f24dc9529376e83d453afdf47
Alessio <alessio.marchese148@gmail.com>
2024-05-24 16:52:52 +0200

    Modificato model per poter ricevere ulteriori dati da todolist

--- src/Shared/DTOS/ToDoLists/GetAll/Model.cs
+++ src/Shared/DTOS/ToDoLists/GetAll/Model.cs
@@ -3,7 +3,7 @@ using Shared.Entities;
 
 namespace Shared.DTOS.ToDoLists.GetAll;
 public record Request();
-public record Response(Guid Id, string Title, bool IsDone);
+public record Response(Guid Id, string Title, bool IsDone, DateTimeOffset Created, string CreatedBy);
 
 public class ResponseProfile : Profile
 {
