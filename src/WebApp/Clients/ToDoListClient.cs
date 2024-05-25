bbf70bf69306a8f0e270d11c4c95d665c30a7811
Alessio <alessio.marchese148@gmail.com>
2024-05-25 17:07:47 +0200

    Implementato il detail delle liste

--- src/WebApp/Clients/ToDoListClient.cs
+++ src/WebApp/Clients/ToDoListClient.cs
@@ -1,4 +1,5 @@
-﻿using Shared.Entities;
+﻿using Shared.DTOS.ToDoLists.GetItemsByListId;
+using Shared.Entities;
 using System.Net.Http.Json;
 
 namespace webapp.Clients;
@@ -7,4 +8,7 @@ public class ToDoListClient
 {
     public async Task<List<ToDoList>> GetAllAsync(HttpClient httpClient)
         => await httpClient.GetFromJsonAsync<List<ToDoList>>("http://localhost:5084/api/todo-list") ?? [];
+
+    public async Task<Response?> GetItemsByListIdAsync(HttpClient httpClient, Guid id)
+        => await httpClient.GetFromJsonAsync<Response>($"http://localhost:5084/api/todo-list/{id}/todo-item"); 
 }
