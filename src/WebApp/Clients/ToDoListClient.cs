4f5691fb768ebbcf5e837d14d2933cb1e5256f0f
Alessio <alessio.marchese148@gmail.com>
2024-05-25 17:15:02 +0200

    Refactoring del codice, ora le implementazioni frontend usano direttamente le response nello shared

--- src/WebApp/Clients/ToDoListClient.cs
+++ src/WebApp/Clients/ToDoListClient.cs
@@ -1,14 +1,13 @@
-﻿using Shared.DTOS.ToDoLists.GetItemsByListId;
-using Shared.Entities;
+﻿using Shared.Entities;
 using System.Net.Http.Json;
 
 namespace webapp.Clients;
 
 public class ToDoListClient
 {
-    public async Task<List<ToDoList>> GetAllAsync(HttpClient httpClient)
-        => await httpClient.GetFromJsonAsync<List<ToDoList>>("http://localhost:5084/api/todo-list") ?? [];
+    public async Task<List<Shared.DTOS.ToDoLists.GetAll.Response>> GetAllAsync(HttpClient httpClient)
+        => await httpClient.GetFromJsonAsync<List<Shared.DTOS.ToDoLists.GetAll.Response>>("http://localhost:5084/api/todo-list") ?? [];
 
-    public async Task<Response?> GetItemsByListIdAsync(HttpClient httpClient, Guid id)
-        => await httpClient.GetFromJsonAsync<Response>($"http://localhost:5084/api/todo-list/{id}/todo-item"); 
+    public async Task<Shared.DTOS.ToDoLists.GetItemsByListId.Response?> GetItemsByListIdAsync(HttpClient httpClient, Guid id)
+        => await httpClient.GetFromJsonAsync<Shared.DTOS.ToDoLists.GetItemsByListId.Response>($"http://localhost:5084/api/todo-list/{id}/todo-item"); 
 }
