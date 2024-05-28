b9f8cc94c7e03562e5f51bc47521f2fe5170e9cc
Antonio Lanzolla <antonio.lanzolla@gmail.com>
2024-05-28 11:24:36 +0200

    Moved entities

---
+++ src/WebApi/Domains/Entities/ToDoItem.cs
@@ -0,0 +1,47 @@
+﻿using System.Text.Json.Serialization;
+using Shared.DTOS.ToDoItems.Update;
+using webapi.Domains.Entities.Common;
+
+namespace webapi.Domains.Entities;
+
+public class ToDoItem : AuditableBaseEntity<Guid>
+{
+    public string Text { get; set; }
+    public Guid ToDoListId { get; set; }
+    [JsonIgnore]
+    public ToDoList ToDoList { get; set; }
+    public bool IsDone { get; set; } = false;
+
+    protected ToDoItem()
+    {
+
+    }
+    [JsonConstructor]
+    protected ToDoItem(Guid id, string text, Guid toDoListId) : base(id)
+    {
+        Text = text;
+        ToDoListId = toDoListId;
+    }
+
+    private ToDoItem(string text, Guid toDoListId) : base()
+    {
+        Text = text;
+        ToDoListId = toDoListId;
+    }
+
+    public static ToDoItem Create(string text, Guid toDoListId)
+    {
+        return new ToDoItem(Guid.NewGuid(), text, toDoListId);
+    }
+
+    public static ToDoItem CreateNoGuid(string text, Guid toDoListId)
+    {
+        return new ToDoItem(text, toDoListId);
+    }
+
+    public static ToDoItem Update(ToDoItem toDoItem, Request r)
+    {
+        toDoItem.IsDone = r.IsDone;
+        return toDoItem;
+    }
+}
---
+++ src/WebApi/Domains/Entities/ToDoList.cs
@@ -0,0 +1,51 @@
+﻿using System.Text.Json.Serialization;
+using webapi.Domains.Entities.Common;
+
+namespace webapi.Domains.Entities;
+
+public class ToDoList : AuditableBaseEntity<Guid>
+{ 
+    public string Title { get; set; }
+    public bool IsDone { get; set; }
+    public List<Guid> ToDoItemsId { get; set; }
+    public List<ToDoItem> ToDoItems { get; set; } = new List<ToDoItem>();
+
+    protected ToDoList()
+    {
+
+    }
+
+    [JsonConstructor]
+    protected ToDoList(Guid id, string title) : base(id)
+    {
+        Title = title;
+    }
+
+    public static ToDoList Create(string title)
+    {
+        return new ToDoList(Guid.NewGuid(), title);
+    }
+
+    public ToDoItem AddToDoItem(string text)
+    {
+        var toDoItem = ToDoItem.CreateNoGuid(text, this.Id);
+        toDoItem.CreatedBy = "Alessio";
+        toDoItem.Created = DateTime.UtcNow;
+        ToDoItems.Add(toDoItem);
+        return toDoItem;
+    }
+
+    public void CheckDone()
+    {
+        foreach(var item in ToDoItems)
+        {
+            if (!item.IsDone) 
+            {
+                IsDone = false;
+                return;
+            }
+        }
+        IsDone = true;
+    }
+}
+
