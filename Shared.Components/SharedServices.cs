8c6173f0448fcd05961aa4e4b674b950f3a2dc40
Alessio <alessio.marchese148@gmail.com>
2024-06-12 15:33:47 +0200

    Creato una libreria per condividere componenti razor e spostato il primo componente

---
+++ Shared.Components/SharedServices.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Shared.Components.Clients;
+
+namespace Shared.Components;
+public static class SharedServices
+{
+	public static void AddMyRclServices(this IServiceCollection serviceCollection)
+	{
+		serviceCollection.AddSingleton<ToDoItemClient>();
+		serviceCollection.AddSingleton<ToDoListClient>();
+	}
+}
--- src/WebApp/Program.cs
+++ src/WebApp/Program.cs
@@ -1,11 +1,13 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Shared.Components;
 using webapp;
 using webapp.Clients;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddMyRclServices();
 builder.Services.AddScoped(sp => new ToDoListClient());
 builder.Services.AddScoped(sp => new ToDoItemClient());
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
